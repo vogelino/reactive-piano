ee1115cb91b33eef811d741e93104c6c31bb569f
vogelino <contact@vogelino.com>
2017-08-07 18:26:58 +0200

    Add key and mouse streams to build the dom

--- src/index.js
+++ src/index.js
@@ -1,21 +1,59 @@
+import xs from 'xstream';
 import { run } from '@cycle/run';
-import { makeDOMDriver, div, input, p } from '@cycle/dom';
+import { makeDOMDriver } from '@cycle/dom';
 import { html } from 'snabbdom-jsx';
 import { insertRoot } from './utils/dom';
+import pianoKeys from './constants/pianoKeys';
+import './index.css';
 
 insertRoot(document.body);
 
-const main = (sources) => {
+const getPianoKeysByEvent = (previousPianoKeys, { keyCode, pressed }) => {
+	const preparedPianoKeys = previousPianoKeys.map((pianoKey) => ({
+		...pianoKey,
+		pressed: (
+			pianoKey.keyboardKeyCode === keyCode ?
+				pressed : pianoKey.pressed
+		),
+	}));
+	return preparedPianoKeys;
+};
+
+const mapKeyEventToPressedState = (pressed) =>
+	({ keyCode }) => ({ pressed, keyCode });
+
+const mapClickEventToPressedState = (pressed) =>
+	({ target }) => ({
+		pressed,
+		keyCode: parseInt(target.getAttribute('id'), 10),
+	});
+
+const main = ({ DOM }) => {
+	const keyDownEv$ = DOM.select('document').events('keydown')
+		.map(mapKeyEventToPressedState(true));
+	const keyUpEv$ = DOM.select('document').events('keyup')
+		.map(mapKeyEventToPressedState(false));
+	const mouseDownEv$ = DOM.select('.piano_key').events('mousedown')
+		.map(mapClickEventToPressedState(true));
+	const mouseUpEv$ = DOM.select('.piano_key').events('mouseup')
+		.map(mapClickEventToPressedState(false));
+	const keyUpAndDown$ = xs.merge(keyDownEv$, keyUpEv$, mouseDownEv$, mouseUpEv$);
+	const keyCodes$ = keyUpAndDown$.fold(getPianoKeysByEvent, pianoKeys);
+
 	const sinks = {
-		DOM: sources.DOM.select('input').events('click')
-			.map((evt) => evt.target.checked)
-			.startWith(false)
-			.map((toggled) => (
-				<div>
-					<input type="checkbox" /> Toggle me
-					<p>{toggled ? 'on' : 'off'}</p>
-				</div>
-			)),
+		DOM: keyCodes$.map((preparedPianoKeys) => (
+			<ul className="piano">
+				{preparedPianoKeys.map(({ keyColor, keyboardKeyCode, pressed }) => (
+					<li
+						id={keyboardKeyCode}
+						className={`
+							piano_key piano_key_${keyColor}
+							piano_key_${keyColor}--${pressed ? 'pressed' : 'unpressed'}`
+						}
+					/>
+				))}
+			</ul>
+		)),
 	};
 
 	return sinks;
